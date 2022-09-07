b1b2aae14075c1b4768f2e647351307878f81d3b
Matheus Jácome <95725545+mfjacome@users.noreply.github.com>
2022-09-06 21:10:00 -0300

    Introdução a lista(vetor/array)

---
+++ arraysJS/introducao.js
@@ -0,0 +1,7 @@
+//Demonstração simples de uma lista(vetor/array):  
+const notas = [10, 5, 3, 8]
+
+//função: .length utilizada para indentificar o tamanho do vetor.
+let media = (notas[0] + notas[1] + notas[2] +notas[3])/ notas.length 
+
+console.log(media)
